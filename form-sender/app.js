4b01d783a1fee84f05319d5101dde6b77e01c16e
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-04-04 22:07:35 +0500

    Дописать вебсайт

---
+++ form-sender/app.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("form");
+const firstName = form.querySelector("#firstName");
+const secondName = form.querySelector("#secondName");
+const age = form.querySelector("#age");
+const formSelect = form.querySelector("select");
+const phone = form.querySelector("#phone");
+const email = form.querySelector("#email");
+const submitButton = form.querySelector("button[type='submit']");
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let genderOption = Array.from(
+    form.querySelectorAll('input[type="radio"]')
+  ).find((r) => r.checked);
+
+  const data = {
+    firstName: firstName.value,
+    secondName: secondName.value,
+    age: age.value,
+    gender: genderOption.value,
+    formSelect: formSelect.value,
+    phone: phone.value,
+    email: email.value,
+  };
+
+  let response = fetch("https://eoyesn7cru0ofv2.m.pipedream.net", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+    });
+});
