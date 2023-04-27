625df98e0f1fcda5e20a77088f31969087616232
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-04-27 19:48:57 +0500

    Change website design completely

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const form = document.querySelector("form");
 const firstName = form.querySelector("#firstName");
 const secondName = form.querySelector("#secondName");
-const age = form.querySelector("#age");
-const formSelect = form.querySelector("select");
-const phone = form.querySelector("#phone");
 const email = form.querySelector("#email");
 const submitButton = form.querySelector("button[type='submit']");
 
@@ -17,21 +14,22 @@ submitButton.addEventListener("click", (e) => {
   const data = {
     firstName: firstName.value,
     secondName: secondName.value,
-    age: age.value,
-    gender: genderOption.value,
-    formSelect: formSelect.value,
-    phone: phone.value,
     email: email.value,
   };
 
   // Создаем и отправляем запрос
-  let response = fetch("https://eoyesn7cru0ofv2.m.pipedream.net", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).then((response) => console.log(response));
+  async function sendRequest() {
+    let response = await fetch("https://eoyesn7cru0ofv2.m.pipedream.net", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log(response);
+  }
+
+  sendRequest();
 });
 
 // https://pipedream.com/@valdemarcheck/requestbin-p_ZJCorAk/inspect
